1649005c2bb7f43c0a3613a925bf4988558a1ca6
Aakanksha <aakanksha0499@gmail.com>
2022-05-17 11:51:22 +0530

    Added_Length_Calculation

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,5 +1,12 @@
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("WELCOME TO LINE");
+        double x1 = Math.floor(Math.random() * 10) % 2;
+        double x2 = Math.floor(Math.random() * 10) % 3;
+        double y1 = Math.floor(Math.random() * 10) % 4;
+        double y2 = Math.floor(Math.random() * 10) % 5;
+        double lengthOfLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+        System.out.println("Length of The Line = " + lengthOfLine);
     }
 }
