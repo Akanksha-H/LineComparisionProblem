06320be577b38b439c6ab562fe42f1ab86db1ca3
Aakanksha <aakanksha0499@gmail.com>
2022-05-17 13:04:26 +0530

    Added-CompareTo_Method

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,22 +1,24 @@
+import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("WELCOME TO LINE");
+        //Two values to compare
+        Double L1, L2;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter end of fist line = ");
+        L1 = sc.nextDouble();
 
-        double x1 = Math.floor(Math.random() * 10) % 2;
-        double x2 = Math.floor(Math.random() * 10) % 3;
-        double y1 = Math.floor(Math.random() * 10) % 4;
-        double y2 = Math.floor(Math.random() * 10) % 5;
+        System.out.print("Enter end of second line = ");
+        L2 = sc.nextDouble();
 
-        double x21 = Math.floor(Math.random() * 10) % 2;
-        double x22 = Math.floor(Math.random() * 10) % 3;
-        double y21 = Math.floor(Math.random() * 10) % 4;
-        double y22 = Math.floor(Math.random() * 10) % 5;
-
-        LineUse Line1 = new LineUse();
-        Line1.lengthOfLine1 = String.valueOf(Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
-
-        LineUse Line2 = new LineUse();
-        Line2.lengthOfLine2 = String.valueOf(Math.sqrt((x22 - x21) * (x22 - x21) + (y22 - y21) * (y22 - y21)));
-        System.out.println(Line1.equals(Line2));
+        //Double compare method
+        if (Double.compare(L1,L2) == 0) {
+            System.out.println("Lines have same length");
+        }
+        else if (Double.compare(L1,L2) < 0)
+            System.out.println("Line 1 is smaller than Line 2");
+        else
+            System.out.println("Line 1 is greater than Line 2");
     }
 }
+
--- src/LineUse.java
+++ src/LineUse.java
@@ -2,10 +2,25 @@ public class LineUse {
 
     public void myClass(){
 
+        String lengthOfLine1;
+
+        String lengthOfLine2;
+
+
+        double x1 = Math.floor(Math.random() * 10) % 2;
+        double x2 = Math.floor(Math.random() * 10) % 3;
+        double y1 = Math.floor(Math.random() * 10) % 4;
+        double y2 = Math.floor(Math.random() * 10) % 5;
+
+        double x21 = Math.floor(Math.random() * 10) % 2;
+        double x22 = Math.floor(Math.random() * 10) % 3;
+        double y21 = Math.floor(Math.random() * 10) % 4;
+        double y22 = Math.floor(Math.random() * 10) % 5;
+
+
+        double lengthOfLine1 = (Math.round(Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))));
+        double lengthOfLine2 = Math.round(Math.sqrt((x22 - x21) * (x22 - x21) + (y22 - y21) * (y22 - y21)));
+
     }
-
-    String lengthOfLine1;
-
-    String lengthOfLine2;
 }
 
